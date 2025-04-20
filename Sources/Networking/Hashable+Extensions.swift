1ebe4a529f9b198eec32ac6b6b236db7450236da
Anton Martinsson <antonm@rtinsson.com>
2025-04-20 12:08:42 +0200

    Update Hashable extension

--- Sources/Networking/Hashable+Extensions.swift
+++ Sources/Networking/Hashable+Extensions.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension Hashable {
-    var queryItems: [URLQueryItem] {
+    var asQueryItems: [URLQueryItem] {
         let mirror = Mirror(reflecting: self)
         return mirror.children.compactMap { child in
             guard let label = child.label else { return nil }
@@ -15,6 +15,27 @@ public extension Hashable {
             let value = "\(child.value)"
             return URLQueryItem(name: label, value: value)
         }
+    }
+    
+    
+    var asHeaders: [String: String] {
+        let mirror = Mirror(reflecting: self)
+        let children: [(String, String)] = mirror.children.compactMap { child in
+            guard let label = child.label else { return nil }
+            let valueMirror = Mirror(reflecting: child.value)
+            if valueMirror.isOptional {
+                guard let valueString = valueMirror.valueString else {
+                    return nil
+                }
+                return (label, valueString)
+            }
+            return (label, "\(child.value)")
+        }
+        
+        let headers = children.reduce(into: [String: String]()) { dict, pair in
+            dict[pair.0] = pair.1
+        }
+        return headers
     }
 }
 
